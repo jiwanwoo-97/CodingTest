05d0a89e08d7ed1aeb9b743d19734796b2d63314
Wanwoo Ji <jwo9767@naver.com>
2022-01-13 21:14:43 +0900

    프로그래머스 LEVEL1 짝수와 홀수 풀이

---
+++ LEVEL1/짝수와 홀수.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class 짝수와 홀수 {
+    class Solution {
+        public String solution(int num) {
+            String answer = "";
+            if(num % 2 == 0) {
+                answer = "Even";
+            } else {
+                answer = "Odd";
+            }
+            return answer;
+        }
+    }
+}
