6e890bbaf226e59504ef4aa58eabef5d092cc4a9
Wanwoo Ji <jwo9767@naver.com>
2022-03-03 23:27:31 +0900

    BAEKJOON 아스키코드 풀이

---
+++ BACKJOON/아스키코드.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scan=new Scanner(System.in);
+        String askii = scan.next();
+        char answer= askii.charAt(0);
+        int anw=(int)answer;
+        System.out.println(anw);
+
+
+
+    }
+
+
+}
